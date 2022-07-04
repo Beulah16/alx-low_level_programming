7a25d1e102862f3b2b1d24e53ba811f9e88b823d
Beulah <igboanugobeulah16@gmail.com>
2022-07-04 16:18:52 -0700

    All about C

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Get the last digit of a number
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main (void)
+{
+	int n;
+	int digit;
+
+	srand (time(0));
+
+	n = rand() - RAND_MAX / 2;
+	digit = n % 10
+
+		if (digit == 0)
+		{
+			printf ("Last digitt of %d is %d and is 0\n", n, digit);
+		}
+		else if (digit > 5)
+		{
+			printf("Last digit of %d is %d and is greater than 5\n", n, digit);
+		}
+		else if (digit < 6 && digit !=0)
+		{
+			printf ("Last digit of %d is %d and is less than 6 and not 0\n", n, digit);
+		}
+	return (0);
+}
+
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description - print alphabets in both lower and upper case
+ * Return: Always 0 (Success)
+ */
+
+int main (void)
+{
+	char small = 'a';
+	char big = 'A';
+
+	while (small <= 'z')
+	{
+		putchar(small);
+		small++;
+	}
+
+	while (big <= 'Z')
+	{
+		putchar(big);
+		big++;
+	}
+	putchar('\n');
+
+	return (0);
+}
