9ab0cbac6d4296e6db504f89b3c706634bded844
Beulah <igboanugobeulah16@gmail.com>
2022-07-04 17:10:03 -0700

    C is fun :|

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: print all combinations of single digits
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	intt num = 0;
+
+	while (num < 10)
+	{
+		putchar(48 + num);
+		if (num != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		
+		}
+		num++;
+	}
+	putchar('\n');
+
+	return (0);
+}
