ce35756be6c28825d8ed498ec762b17c1c80baff
Beulah <igboanugobeulah16@gmail.com>
2022-07-04 17:31:37 -0700

    Advanced tinz baby!

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: print all possible two digits combinations
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int first;
+	int second = 0;
+
+	while (second <=9)
+	{
+		first = 0;
+
+		while (first <= 9)
+		{
+			if (first != second && second < first)
+			{
+				putchar('0' + second);
+				putchar('0' + first);
+				if (first + second != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			first++;
+		}
+		second++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
