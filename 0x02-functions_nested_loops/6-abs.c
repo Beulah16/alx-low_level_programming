5a771f908750c9ba706e72cbe2a406e9b33c7e55
Beulah <igboanugobeulah16@gmail.com>
2022-07-08 02:25:55 -0700

    C is :(

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @num: the number to be converted to its absolute
+ * Return: returns an integer
+ */
+
+int _abs(int num)
+{
+	int abs = num * ((num > 0) - (num < ));
+
+	return (abs);
+}
