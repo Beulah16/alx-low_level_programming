4b7580c56f1d90a37c3ee3b3134baeec7a51e130
Beulah <igboanugobeulah16@gmail.com>
2022-07-09 16:27:01 -0700

    More loops

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+nclude "main.h"
+
+/**
+ * print_triangle - check
+ * @size : integer type
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = i; j <= size; j++)
+		{
+			if (j <= (size - 1))
+			{
+				_putchar(' ');
+			}
+			else
+				_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
