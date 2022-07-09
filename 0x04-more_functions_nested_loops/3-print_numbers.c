db01e55763c906712d8c6d7bba0a67cd0fb175dd
Beulah <igboanugobeulah16@gmail.com>
2022-07-09 15:40:40 -0700

    More loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - entry point
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		_putchar('0' + num);
+		num++;
+	}
+	_putchar('\n');
+}
