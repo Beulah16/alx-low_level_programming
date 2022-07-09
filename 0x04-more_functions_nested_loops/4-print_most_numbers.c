c0f022e23f04245222da1018df012d9a244dbb4b
Beulah <igboanugobeulah16@gmail.com>
2022-07-09 15:45:25 -0700

    More loops

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers -print numbers except 2 and 4
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	char num = 0;
+
+	while (num < 10)
+	{
+		if (num != 2 && num != 4)
+		{
+			_putchar('0' + num);
+		}
+		num++;
+	}
+	_putchar('\n');
+}
