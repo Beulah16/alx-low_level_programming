25e1138068c3c3c2878d7aa99dac75bd8c9a7434
Beulah16 <igboanugobeulah16@gmail.com>
2023-09-12 16:09:36 -0700

    Alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * print_alphabet - A function
  *
  * Return: Always 0
  */
 void print_alphabet()
 {
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c);
