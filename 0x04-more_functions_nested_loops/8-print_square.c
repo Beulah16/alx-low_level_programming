04472de6e2fcf9fa47f8cb3b6d77325ab417e62f
Beulah16 <igboanugobeulah16@gmail.com>
2023-09-14 13:20:16 -0700

    Squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "man.h"
+
+/**
+ * print_square - A function
+ * @size: parameter to be evaluated
+ *
+ * Return: void
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else 
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
