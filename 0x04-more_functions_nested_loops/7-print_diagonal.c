40a6d9bbee6fb61c3ca73b054be25335d9bb8617
Beulah16 <igboanugobeulah16@gmail.com>
2023-09-14 10:50:04 -0700

    Diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -16,14 +16,14 @@ void print_diagonal(int n)
 		i = 0;
 		while (i < n)
 		{
-			_putchar('\\');
-			_putchar('\n');
 			j = 0;
-			while (j <= i)
+			while (j < i)
 			{
 				_putchar(' ');
 				j++;
 			}
+			_putchar('\\');
+			_putchar('\n');
 			i++;
 		}
 		_putchar('\n');
