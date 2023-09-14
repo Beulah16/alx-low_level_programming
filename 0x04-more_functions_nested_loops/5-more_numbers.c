417197c2a8e9ab9f363a860d4f4abfb7c8a02d3e
Beulah16 <igboanugobeulah16@gmail.com>
2023-09-14 09:32:18 -0700

    0 to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - A function
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i <= 10)
+	{
+		j = 0;
+		while (j <= 14)
+		{
+			_putchar(j + '0');
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+	_putchar('\n');
+}
