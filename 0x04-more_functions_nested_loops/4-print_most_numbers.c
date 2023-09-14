8e0fad05b3130559e2b28177325be29ff725c5bb
Beulah16 <igboanugobeulah16@gmail.com>
2023-09-14 09:15:49 -0700

    Most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - A function
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while ( i <= 9)
+	{
+		if (i == 2 || i == 4)
+		{
+			i++;
+			continue;
+		}
+		_putchar(i + '0');
+		i++;
+	}
+	_putchar('\n');
+}
