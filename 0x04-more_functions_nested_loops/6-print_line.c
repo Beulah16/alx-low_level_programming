6eb08b9feaee07682ed25a7e2d106d63314f53b9
Beulah16 <igboanugobeulah16@gmail.com>
2023-09-14 09:59:21 -0700

    Lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_line - A funtion
+ * @n: parameter to be evaluated
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		int i;
+
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\\');
+		_putchar('n');
+	}
+	else
+	{
+		_putchar('\\');
+		_putchar('n');
+	}
+}
