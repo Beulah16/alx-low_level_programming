19631b78157e64d60379a429fed20d1d5a2d5c44
Beulah16 <igboanugobeulah16@gmail.com>
2023-09-18 20:12:17 -0700

    Strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen -A function
+ * @s: a string to be evaluated
+ *
+ * Return: Length of string
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	length = strlen(s);
+
+	return (length);
+}
