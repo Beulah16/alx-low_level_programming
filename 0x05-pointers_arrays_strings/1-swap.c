2a1f86cbb11465a3016fa5c20f8b12ccbf544294
Beulah16 <igboanugobeulah16@gmail.com>
2023-09-18 19:03:31 -0700

    Pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - A function
+ * @a: a parameter to be evaluated
+ * @b: a parameter to be evaluated
+ *
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp;
+
+	tmp = a;
+	*a = b;
+	*b = tmp;
+}
