56bf76eeaec162b45a7986b861ba4e609338c6dd
Beulah16 <igboanugobeulah16@gmail.com>
2023-09-18 19:16:42 -0700

    Pointers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -10,8 +10,12 @@
 void swap_int(int *a, int *b)
 {
 	int *tmp;
+	int *swp1;
+	int *swp2;
 
-	tmp = a;
-	*a = b;
-	*b = tmp;
+	*swp1 = a;
+	*swp2 = b;
+	*tmp = swp1;
+	*swp1 = swp2;
+	*swp2 = tmp;
 }
