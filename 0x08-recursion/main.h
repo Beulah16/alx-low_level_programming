746af85a5fd331ec7220383f6456fb75932d8142
Beulah16 <igboanugobeulah16@gmail.com>
2023-10-02 05:50:34 -0700

    Recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,5 +7,9 @@ void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int _find_sqrt(int a, int b);
+int is_prime_number(int n);
+int _divisible(int a, int b);
 
 #endif
