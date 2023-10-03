66bb7664f832187ca18eda8b817165d1f1a93f45
Beulah16 <igboanugobeulah16@gmail.com>
2023-10-03 06:53:51 -0700

    Arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -  a program that the number of arguments passed into it
+ * @argc: a parameter
+ * @argv: a parameter
+ *
+ * Return: 0 always
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int mul;
+
+	if (argc > 0)
+	{
+		mul = 1;
+		for (i = 1; i < argc; i++)
+		{
+			mul *= argv[i];
+		}
+		printf("%d\n", mul);
+		return (0);
+	}
+	printf("Error\n");
+
+	return (1);
+}
