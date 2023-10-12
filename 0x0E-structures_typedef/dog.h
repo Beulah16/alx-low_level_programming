7b0edd9e0b05c0ecf01b51cee5cbb287fe150fac
Beulah16 <igboanugobeulah16@gmail.com>
2023-10-11 20:11:59 -0700

    Puppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,18 @@
-#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
 
 /**
- * struct dog - a new struct
- * @name: string
- * @age: float
- * @owner: string
- *
- * Description: A new type
+ * struct dog - a new type
+ * @name: the name
+ * @age: the age
+ * @owner: the owner
+ * Description: a new struct
  */
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif
