32c30401dcccd7d72bdb99faf6fa434075b63000
Beulah <igboanugobeulah16@gmail.com>
2023-11-23 01:31:32 -0800

    My product

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program that multiplies two numbers.
+ * @argc: number of CL parameters
+ * @argv: array of CL parameters
+ *
+ * Return: 0 always
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int j;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (-1);
+	}
+
+	i = atoi(argv[1]);
+	j = atoi(argv[2]);
+	printf("%d\n", i*j);
+	return (0);
+}
