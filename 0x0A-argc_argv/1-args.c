bc294a4132d25481626e842838897604d903eac0
Beulah <igboanugobeulah16@gmail.com>
2023-11-23 00:46:20 -0800

    My number

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints the number of arguments passed into it
+ * @argc: number of CL parameters
+ * @argv: array of CL parameters
+ *
+ * Return: 0 always
+ */
+
+int main(int argc, char* argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
