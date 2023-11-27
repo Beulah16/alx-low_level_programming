299cf010e1f4b1e275fe602b7e87f7878cd036aa
Beulah <igboanugobeulah16@gmail.com>
2023-11-27 08:44:45 -0800

    New strcut

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Define a new type struct dog
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ * Description: A new structure
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
