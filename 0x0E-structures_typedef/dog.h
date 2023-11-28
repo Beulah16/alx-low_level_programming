209781f72d12c848174af127a4634df5d73eb5c7
Beulah <igboanugobeulah16@gmail.com>
2023-11-27 19:23:59 -0800

    Print dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,12 +8,12 @@
  * @owner: Dog's owner
  * Description: A new structure
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
