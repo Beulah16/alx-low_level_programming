8dbda6cd91f2dcf487397fff9d8173e34744b1d6
Beulah <igboanugobeulah16@gmail.com>
2023-12-11 15:47:30 -0800

    Number of elements

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * list_len - a function that prints all the elements of a list_t list
+ * @h: head node
+ *
+ * Return: size of list
+ */
+
+size_t list_len(const list_t *h)
+{
+	const list_t *temp;
+	size_t count;
+
+	temp = h;
+	count = 0;
+	while (temp != NULL)
+	{
+		count++;
+		temp = temp->next;
+	}
+	return (count);
+}
