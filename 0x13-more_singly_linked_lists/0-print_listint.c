4431643d840bdf8fbba316832fb945be3ce2324c
Beulah <igboanugobeulah16@gmail.com>
2023-12-12 14:40:37 -0800

    Int elements

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_listint - a function that prints all the elements of a listint_t list
+ * @h: head node of the list
+ *
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *temp;
+	size_t count;
+
+	temp = h;
+	count = 0;
+	while(temp != NULL)
+	{
+		printf("%u\n", temp->n);
+		count++;
+		temp = temp->next;
+	}
+	return (count);
+}
+
