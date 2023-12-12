ab543834b5cecc68a86e230f66d925fe381d84f1
Beulah <igboanugobeulah16@gmail.com>
2023-12-12 14:59:03 -0800

    Betty!

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * listint_len - a function that returns the number of elements in a linked list
+ * listint_len - a function that returns the number of elements in a list
  * @h: head node of the list
  *
  * Return: number of elements
