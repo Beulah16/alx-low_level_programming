cb472a9cd49e4c29f69754bfcd1f0789447f275a
Beulah <igboanugobeulah16@gmail.com>
2023-12-12 14:57:00 -0800

    Number of elements

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * listint_len - a function that returns the number of elements in a linked list
+ * @h: head node of the list
+ *
+ * Return: number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *temp;
+	size_t num;
+
+	temp = h;
+	num = 0;
+	while (temp != NULL)
+	{
+		num++;
+		temp = temp->next;
+	}
+	return (num);
+}
+
