44fed68f6968b8958d529dddc5ce3213145ae26e
Beulah <igboanugobeulah16@gmail.com>
2023-12-12 02:41:21 -0800

    handling null case

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,12 +13,19 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new;
 
 	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
 	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
 	new->len = strlen(str);
 	new->next = *head;
 	*head = new;
-	if (*head)
-		return (*head);
-	else
-		return (NULL);
+	
+	return (new);
 }
