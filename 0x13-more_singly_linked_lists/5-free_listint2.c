e94d72f69b8a5a550f42e99145c6c5a614a278eb
Beulah <igboanugobeulah16@gmail.com>
2023-12-13 03:04:58 -0800

    free blocks

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - a function that frees a listint_t list
+ * @head: head node
+ *
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (*head != NULL)
+	{
+		temp = *head;
+	}
+	while (temp->next != NULL)
+	{	
+		free(temp);
+		temp = temp->next;
+	}
+	*head = temp;
+	free(*head);
+}
